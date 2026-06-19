582a2e8ddd8593b8345ce6370184f71775b888ae
busrakkan <busra.akkan.d@gmail.com>
2026-06-19 14:46:41 +0200

    server: add cors and morgan, basic config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-// imports
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-// init express
-const app = new express();
+const app = express();
 const port = 3001;
 
-// activate the server
+app.use(express.json());
+app.use(morgan("dev"));
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
